8cefb56c6a6ce933fdc96c45440c0290e531e170
JuanCamiloGiraldo <juan_giraldo@itis.com.co>
2023-10-20 11:40:27 -0500

    Resolví el ejercicio sin usar getters, setters, ni polimorfismo

--- Cajero/Cajero.java
+++ Cajero/Cajero.java
@@ -1,4 +1,17 @@
 package Cajero;
+import java.util.Scanner;
+
+import logica.logica_cajero;
+
 public class Cajero {
     
+ 
+    public static void main(String[] args) {
+        System.out.println("Bienvenido al cajero, presione cualquier tecla para continuar");
+        Scanner entrada = new Scanner(System.in);
+        int digito = entrada.nextInt();    
+        logica_cajero logica = new logica_cajero(digito);
+        logica.info();
+
+    }
 }
--- logica/logica_cajero.java
+++ logica/logica_cajero.java
@@ -3,21 +3,25 @@ package logica;
 import java.util.Scanner;
 
 public class logica_cajero {
-    protected int saldo = 0;
+    private int saldo = 0, digito;
     Scanner entrada = new Scanner(System.in);
 
-    public logica_cajero(int saldo){
-        this.saldo = saldo;
+    public logica_cajero(int digito){
+        this.digito = digito;
+
     }
-    private void info(){
-        System.out.println("-------------------------------------------------------");
-        System.out.println("Por favor, seleccione una opción");
-        System.out.println("1. Consulta tu saldo");
-        System.out.println("2. Retiro de efectivo");
-        System.out.println("3. Depósito de efectivo");
-        System.out.println("4. Salir");
-        System.out.println("--------------------------------------------------------");
-        int digito = entrada.nextInt();
+       public void info(){
+        do{
+            Scanner entrada = new Scanner(System.in);
+
+            System.out.println("-------------------------------------------------------");
+            System.out.println("Por favor, seleccione una opción");
+            System.out.println("1. Consulta tu saldo");
+            System.out.println("2. Retiro de efectivo");
+            System.out.println("3. Depósito de efectivo");
+            System.out.println("4. Salir");
+            System.out.println("--------------------------------------------------------");
+            int digito = entrada.nextInt();
         switch (digito) {
             case 1:
                 consultar();
@@ -28,24 +32,34 @@ public class logica_cajero {
                 consignar_saldo();
             case 4:
                 salir();
-            default:
                 break;
+            default:
+                System.out.println("ha ingresado una opción invalida: " + digito + " no es una entrada correcta");
         }
+        }while(digito != 4);
     }
     private void consignar_saldo(){
-        int consignacion = entrada.nextInt();
-        saldo = saldo + consignacion;
+        System.out.println("tu saldo actual es de: " + saldo + "$ Pesos ¿Cuanto deseas depositar?");
 
+        int consignacion = entrada.nextInt();
+        
+        saldo = saldo + consignacion;
+        info();
     }
     private void retirar(){
+        System.out.println("su saldo actual es de: " + saldo + "$ Pesos, ingrese cuanto desea retirar" );
         int retiro = entrada.nextInt();
+    
         if(retiro > saldo ){
-            System.out.println("no tienes suficiente dinero para realizar el retiro");
-        }else{ saldo = saldo - retiro;}
-        
+            System.out.println("no tienes suficiente dinero para realizar el retiro, tu saldo actual es de" + saldo  );
+        }else{ saldo = saldo - retiro;
+            System.out.println("retiro exitoso, su nuevo saldo es de " + saldo + "$ pesos");
+        }
+        info();
         }
     private void consultar(){
-        System.out.println(saldo);
+        System.out.println("su saldo actual es de: "  + saldo);
+        info();
     }
     private void salir(){
         System.out.println("Gracias por elegirnos");
