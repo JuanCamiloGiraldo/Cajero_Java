1f92cb2e8dcc46fb5a345105cc2084c4c7cdb3e1
JuanCamiloGiraldo <juan_giraldo@itis.com.co>
2023-10-20 15:33:32 -0500

    Agregué la documentación del ejercicio

--- Cajero/Cajero.java
+++ Cajero/Cajero.java
@@ -1,16 +1,19 @@
 package Cajero;
 import java.util.Scanner;
-
+// importo logica_cajero de logica para usarlo
 import logica.logica_cajero;
 
 public class Cajero {
     
  
     public static void main(String[] args) {
+        //Mensaje de bienvenida que solo se mostrará una vez
         System.out.println("Bienvenido al cajero, presione cualquier tecla para continuar");
         Scanner entrada = new Scanner(System.in);
         int digito = entrada.nextInt();    
+        //envío el valor de dígito a través del operador designado "logica" 
         logica_cajero logica = new logica_cajero(digito);
+        //inicializo info, para que el programa comience a recibir instrucciones.
         logica.info();
 
     }
--- logica/logica_cajero.java
+++ logica/logica_cajero.java
@@ -3,13 +3,15 @@ package logica;
 import java.util.Scanner;
 
 public class logica_cajero {
+    // declaro como privado el saldo y el dígito
     private int saldo = 0, digito;
     Scanner entrada = new Scanner(System.in);
-
+    //Inicializo la variable digito como publica
     public logica_cajero(int digito){
         this.digito = digito;
 
     }
+    // genero el bucle principal en un ciclo do while
        public void info(){
         do{
             Scanner entrada = new Scanner(System.in);
@@ -22,10 +24,10 @@ public class logica_cajero {
             System.out.println("4. Salir");
             System.out.println("--------------------------------------------------------");
             int digito = entrada.nextInt();
+        //usé switch case para ejecutar las lineas de código, si el digito es igual a 4, el bucle finalizará
         switch (digito) {
             case 1:
                 consultar();
-                break;
             case 2:
                 retirar();
             case 3:
@@ -38,6 +40,8 @@ public class logica_cajero {
         }
         }while(digito != 4);
     }
+    // el método consignar saldo usa la variable consignación para guardar el valor ingresado por teclado
+    // después modifico la variable saldo sumandole la consignación
     private void consignar_saldo(){
         System.out.println("tu saldo actual es de: " + saldo + "$ Pesos ¿Cuanto deseas depositar?");
 
@@ -46,6 +50,7 @@ public class logica_cajero {
         saldo = saldo + consignacion;
         info();
     }
+    // retirar es lo mismo que consignar, solo que en lugar de sumar, resta y solo en el caso de que el saldo sea mayor al valor retirado
     private void retirar(){
         System.out.println("su saldo actual es de: " + saldo + "$ Pesos, ingrese cuanto desea retirar" );
         int retiro = entrada.nextInt();
@@ -57,10 +62,12 @@ public class logica_cajero {
         }
         info();
         }
+        //consultar muestra el saldo
     private void consultar(){
         System.out.println("su saldo actual es de: "  + saldo);
         info();
     }
+    //salir hace imprime un mensaje de despedida, al ser la opción 4 terminará el ciclo del programa
     private void salir(){
         System.out.println("Gracias por elegirnos");
     }
