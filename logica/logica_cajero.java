6cd076d56841ec3259c993b1faf53486088c6138
JuanCamiloGiraldo <juan_giraldo@itis.com.co>
2023-10-20 17:01:30 -0500

    Documenté los objetos

--- logica/logica_cajero.java
+++ logica/logica_cajero.java
@@ -6,13 +6,15 @@ public class logica_cajero {
     private int saldo = 0, digito = 0;
     Scanner entrada = new Scanner(System.in);
 
-    public logica_cajero(int digito){
+    public logica_cajero(int digito) {
         this.digito = digito;
-        
+
     }
-        int numero = 0;
-       public void info(){
-        do{
+
+    int numero = 0;
+
+    public void info() {
+        do {
             Scanner entrada = new Scanner(System.in);
 
             System.out.println("-------------------------------------------------------");
@@ -23,50 +25,55 @@ public class logica_cajero {
             System.out.println("4. Salir");
             System.out.println("--------------------------------------------------------");
             int digito = entrada.nextInt();
-        switch (digito) {
-            case 1:
-                consultar();
-                break;
-            case 2:
-                retirar();
-                break;
-            case 3:
-                consignar_saldo();
-            case 4: 
-                salir();
-                numero = 4;
-                break;
-            default:
-                System.out.println("ha ingresado una opción invalida: " + digito + " no es una entrada correcta");
-                break;
+            switch (digito) {
+                case 1:
+                    consultar();
+                    break;
+                case 2:
+                    retirar();
+                    break;
+                case 3:
+                    consignar_saldo();
+                case 4:
+                    salir();
+                    numero = 4;
+                    break;
+                default:
+                    System.out.println("ha ingresado una opción invalida: " + digito + " no es una entrada correcta");
+                    break;
             }
-        }while(numero != 4);
+        } while (numero != 4);
     }
-    private void consignar_saldo(){
+
+    private void consignar_saldo() {
         System.out.println("tu saldo actual es de: " + saldo + "$ Pesos ¿Cuanto deseas depositar?");
 
         int consignacion = entrada.nextInt();
-        
+
         saldo = saldo + consignacion;
         info();
     }
-    private void retirar(){
-        System.out.println("su saldo actual es de: " + saldo + "$ Pesos, ingrese cuanto desea retirar" );
+
+    private void retirar() {
+        System.out.println("su saldo actual es de: " + saldo + "$ Pesos, ingrese cuanto desea retirar");
         int retiro = entrada.nextInt();
-    
-        if(retiro > saldo ){
-            System.out.println("no tienes suficiente dinero para realizar el retiro, tu saldo actual es de" + saldo  );
-        }else{ saldo = saldo - retiro;
+
+        if (retiro > saldo) {
+            System.out.println("no tienes suficiente dinero para realizar el retiro, tu saldo actual es de" + saldo);
+        } else {
+            saldo = saldo - retiro;
             System.out.println("retiro exitoso, su nuevo saldo es de " + saldo + "$ pesos");
         }
         info();
-        }
-    private void consultar(){
-        System.out.println("su saldo actual es de: "  + saldo);
+    }
+
+    private void consultar() {
+        System.out.println("su saldo actual es de: " + saldo);
         info();
     }
-    private void salir(){
-       
+
+    private void salir() {
+
         System.out.println("Gracias por elegirnos");
     }
 }
